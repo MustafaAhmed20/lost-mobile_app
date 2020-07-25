9b117479c1d34f63645c5cd7a9fb9054cfb1505b
Mustafa Ahmed <mmustafaadn@gmail.com>
2020-07-25 03:10:33 +0200

    add snackBars - make login first before add - modify PopupMenu

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 
 import 'homeData.dart';
 import 'wait.dart';
 import 'forms.dart';
+import 'snackBars.dart';
 
 // import the app data
 import 'package:lost/models/appData.dart';
@@ -25,9 +27,14 @@ class _HomeState extends State<Home> {
 
   PageController _pageController;
 
+  // form key
+  GlobalKey<FormBuilderState> _fbKey;
+
   @override
   void initState() {
     super.initState();
+
+    _fbKey = GlobalKey<FormBuilderState>();
 
     _pageController = PageController(initialPage: 0, keepPage: true);
 
@@ -37,6 +44,8 @@ class _HomeState extends State<Home> {
       Provider.of<TypeOperationData>(context, listen: false).loadData();
       // load the Status Operation
       Provider.of<StatusOperationData>(context, listen: false).loadData();
+      // load the age ranges
+      Provider.of<AgeData>(context, listen: false).loadData();
     }
 
     fetchData();
@@ -62,43 +71,71 @@ class _HomeState extends State<Home> {
       isLoading = false;
     }
 
+    // login state - true if user logged-in
+    bool logged = Provider.of<UserData>(context, listen: true).token == null
+        ? false
+        : true;
+
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showDialog(
-            context: context,
-            builder: (BuildContext context) {
-              return Dialog(
-                child: operatioForm(),
-              );
-            },
-          );
-        },
-        child: Text(
-          '+',
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
+      floatingActionButton: isLoading || types.isEmpty
+          ? null
+          : Builder(
+              // user Builder to have new context under Scaffold
+              builder: (BuildContext context) => FloatingActionButton(
+                onPressed: () {
+                  // login if not already logged-in
+                  logged
+                      ? showDialog(
+                          barrierDismissible: false,
+                          context: context,
+                          builder: (BuildContext context) {
+                            return Dialog(
+                              // show add form with key and current type page
+                              child: operatioForm(
+                                  context, _fbKey, types[_currentPage]),
+                            );
+                          },
+                        )
+                      : // loggin page
+
+                      Navigator.pushNamed(context, '/login',
+                          arguments: {'showAlert': true}).then((value) {
+                          bool logged =
+                              Provider.of<UserData>(context, listen: false)
+                                          .token ==
+                                      null
+                                  ? false
+                                  : true;
+                          // if logged-in show snakebar
+                          if (logged) {
+                            Scaffold.of(context)
+                                .showSnackBar(successLoginSnackBar);
+                          }
+                        });
+                },
+                child: Text(
+                  '+',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
       appBar: AppBar(
         actions: <Widget>[
-          InkWell(
-              onTap: () {
-                Navigator.pushNamed(context, '/login');
+          Builder(
+            builder: (BuildContext context) => PopupMenuButton<String>(
+              onSelected: (value) => handleClick(value, context),
+              itemBuilder: (BuildContext context) {
+                return [
+                  PopupMenuItem<String>(
+                    value: logged ? 'Logout' : 'Login',
+                    child: logged ? Text('Logout') : Text('Login'),
+                  ),
+                ];
               },
-              child: Icon(Icons.account_circle)),
-          PopupMenuButton<String>(
-            onSelected: handleClick,
-            itemBuilder: (BuildContext context) {
-              return {'Logout', 'Settings'}.map((String choice) {
-                return PopupMenuItem<String>(
-                  value: choice,
-                  child: Text(choice),
-                );
-              }).toList();
-            },
+            ),
           ),
         ],
         title: Text('Home'),
@@ -174,12 +211,22 @@ class _HomeState extends State<Home> {
   }
 }
 
-void handleClick(String value) {
-  // switch (value) {
-  //   case 'Logout':
-  //     break;
-  //   case 'Settings':
-  //     break;
-  // }
-  print(value);
+void handleClick(String value, context) {
+  if (value == 'Login') {
+    Navigator.pushNamed(context, '/login').then((value) {
+      bool logged = Provider.of<UserData>(context, listen: false).token == null
+          ? false
+          : true;
+      // if logged-in show snakebar
+      if (logged) {
+        Scaffold.of(context).showSnackBar(successLoginSnackBar);
+      }
+    });
+  }
+
+  if (value == 'Logout') {
+    // logout the user
+    Provider.of<UserData>(context, listen: false).logut();
+    Scaffold.of(context).showSnackBar(successLogoutSnackBar);
+  }
 }
