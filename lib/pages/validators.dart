2af9ca297b5dabd9af2f08b3ce8125647db2afb6
Mustafa <mmustafaadn@gmail.com>
2020-09-02 05:23:36 +0200

    add 'validatPlateNumberNumbers' and 'validatPlateNumberLetters' funcs

--- lib/pages/validators.dart
+++ lib/pages/validators.dart
@@ -43,3 +43,51 @@ String validatPassword(BuildContext context, password) {
   }
   return null;
 }
+
+String validatPlateNumberNumbers(BuildContext context, String value) {
+  // return error massege if not pass the validation else null
+
+  // max length of plate numbers
+  if (value.length > 4) {
+    return "numbers can't be more than 4";
+  }
+
+  //must contain only numbers
+  String pattren = r'^[1-9]{1,4}$';
+  // arabic numbers
+  String pattren2 = r'^[\u0660-\u0669]{1,4}$';
+  RegExp exp1 = RegExp(pattren);
+  RegExp exp2 = RegExp(pattren2);
+
+  if (!exp1.hasMatch(value) && !exp2.hasMatch(value)) {
+    // not have digits
+    return 'only numbers';
+  }
+
+  // success
+  return null;
+}
+
+String validatPlateNumberLetters(BuildContext context, String value) {
+  // return error massege if not pass the validation else null
+
+  // max length of plate numbers
+  if (value.length > 4) {
+    return "letters can't be more than 4";
+  }
+
+  // must contain only letters (Arabic or English)
+  // arabic
+  String pattren1 = r'^[\u0621-\u064A]{1,4}$';
+  String pattren2 = r'^[a-zA-Z]{1,4}$';
+  RegExp exp1 = RegExp(pattren1);
+  RegExp exp2 = RegExp(pattren2);
+
+  if (!exp1.hasMatch(value) && !exp2.hasMatch(value)) {
+    // not have digits
+    return 'only letters';
+  }
+
+  // success
+  return null;
+}
